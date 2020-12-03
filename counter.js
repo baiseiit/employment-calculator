0e792cd88f444c75c3a9359e63917a4b1e933dd0
Alibek <71123014+baiseiit@users.noreply.github.com>
2020-12-03 23:11:36 +0600

    Added studied hours clause

--- counter.js
+++ counter.js
@@ -1,16 +1,29 @@
 function count() {
 	let hour = document.getElementById("hour").value;
+	let studiedHours = document.getElementById("studiedHours").value;
 	let result = document.getElementById("result");
-
-	if (hour < 1 || hour > 168) return;
 
 	const REQUIRED_HOUR_MIDDLE = 2000;
 
-	let resultInDays = (REQUIRED_HOUR_MIDDLE / hour) * 7;
+	if (studiedHours >= REQUIRED_HOUR_MIDDLE) {
+		result.innerHTML = "Сіз дайын мамансыз!";
+		return;
+	}
+
+	if (hour < 1 || hour > 168) return;
+
+	let resultInDays = ((REQUIRED_HOUR_MIDDLE - studiedHours) / hour) * 7;
 
 	if (resultInDays >= 365) {
-		result.innerHTML = Math.round(resultInDays / 365) + " year";
+		result.innerHTML = Math.round(resultInDays / 365) + " жыл";
 	} else {
-		result.innerHTML = Math.round(resultInDays / 30) + " month";
+		let necessaryMonths = Math.round(resultInDays / 30);
+
+		if (necessaryMonths == 0) {
+			result.innerHTML = "Сіз дайын мамансыз!";
+			return;
+		}
+
+		result.innerHTML = necessaryMonths + " ай";
 	}
 }
